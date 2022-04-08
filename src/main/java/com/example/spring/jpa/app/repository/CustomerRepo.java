2a5903693da6262ef923f53989907d9eedd973fd
nilangekar-ni3 <nilangekar.ni3@gmail.com>
2022-04-09 00:51:21 +0530

    Query Changes done

--- src/main/java/com/example/spring/jpa/app/repository/CustomerRepo.java
+++ src/main/java/com/example/spring/jpa/app/repository/CustomerRepo.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface CustomerRepo extends JpaRepository<Customer, Integer> {
 
-    @Query("SELECT new com.example.spring.jpa.app.controller.CustomerResponse(c.name, p.prodName) FROM Customer c JOIN Product p")
+    @Query("SELECT new com.example.spring.jpa.app.controller.CustomerResponse(c.name, p.prodName) FROM Customer c JOIN c.products p")
     public List<CustomerResponse> getJoinInformation();
 }
